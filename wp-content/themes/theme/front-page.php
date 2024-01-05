3e67e1b289cda185117f91e3c66592f6645019cb
Albin Joseph <albin.joseph@yatnam.com>
2024-01-05 18:24:13 +0530

    #2 Home Page Added.

--- wp-content/themes/theme/front-page.php
+++ wp-content/themes/theme/front-page.php
@@ -13,23 +13,19 @@
                                 <?php if($hero['link']):?>
                                         <h5><a href="<?php echo $hero['link'];?>" class="btn-primaryc plr-25"><b><?php echo $hero['link_text'];?></</b></a></h5>
                                 <?php endif; ?>
-                                </div><!-- dplay-tbl-cell -->
-                </div><!-- dplay-tbl -->
-        </div><!-- container -->
+                                </div>
+                </div>
+        </div>
 </section>
-
 
 <section class="story-area left-text center-sm-text pos-relative">
         <div class="abs-tbl bg-2 w-20 z--1 dplay-md-none"></div>
         <div class="abs-tbr bg-3 w-20 z--1 dplay-md-none"></div>
         <div class="container">
                 <div class="heading">
-                        <!-- <img class="heading-img" src="<?php // bloginfo('template_directory');?>/images/heading_logo.png" alt=""> -->
                         <img class="heading-img" src="<?php echo $main_information['section_icon']['url']; ?>" alt=""> 
                         <h2><?php echo $main_information['information_title']; ?></h2>
-
                 </div>
-
                 <div class="row">
                         <div class="col-md-6">
                                 <?php  echo $main_information['left_side'];?>
@@ -42,163 +38,163 @@
         </div><!-- container -->
 </section>
 
+<?php
+    if (have_rows('seller_section')):
+        while (have_rows('seller_section')): the_row();?>
+            <section class="story-area bg-seller color-white pos-relative">
+                <div class="pos-bottom triangle-up"></div>
+                <div class="pos-top triangle-bottom"></div>
+                <div class="container">
+                    <?php
+                        $best_seller_title = get_sub_field('best_seller_title');
+                        $best_seller_icon = get_sub_field('best_seller_icon');
 
-
-<?php if (have_rows('seller_section')): ?>
-    <?php while (have_rows('seller_section')): the_row(); ?>
-        <section class="story-area bg-seller color-white pos-relative">
-            <div class="pos-bottom triangle-up"></div>
-            <div class="pos-top triangle-bottom"></div>
-            <div class="container">
-                <?php $best_seller_title = get_sub_field('best_seller_title'); ?>
-                <?php $best_sellers = get_sub_field('best_sellers'); ?>
-
-                
-                <?php if ($best_seller_title): ?>
-                    <div class="heading">
-
-                        <?php $icon = get_sub_field('best_seller_icon'); ?>     
-                        <!-- <img class="heading-img" src="<?php //bloginfo('template_directory'); ?>/images/heading_logo.png" alt=""> -->
-                        <img class="heading-img" src="<?php echo $icon['url']; ?>" alt="<?php echo esc_attr($title); ?>"> 
-
-                        <h2><?php echo esc_html($best_seller_title); ?></h2>
-                    </div>
-                <?php endif; ?>
-
-                <?php if ($best_sellers): ?>
-                        <div class="row">   
-                                <?php while( have_rows('best_sellers') ): the_row();
-                                        $image = get_sub_field('image');
-                                        $title = get_sub_field('title');
-                                        $price = get_sub_field('price');
-                                        $link = get_sub_field('link');
-                                        $offer = get_sub_field('offer');
-                                        $deal = get_sub_field('deal_of_the_day');
-                                ?>
-                                <div class="col-lg-3 col-md-4 col-sm-6">
-                                        <div class="center-text mb-30">
-                                        <div class="ïmg-200x mlr-auto pos-relative">
-                                                <?php
-                                                switch ($deal) {
-                                                case 'offer':?>
-                                                        <h6 class="ribbon-cont"><div class="ribbon primary"></div><b>OFFER</b></h6>
-                                                        <?php break;
-                                                case 'speciality':?>
-                                                        <h6 class="ribbon-cont"><div class="ribbon secondary"></div><b>SPECIALITY</b></h6>
-                                                        <?php break;
-                                                case 'plus_size':?>
-                                                        <h6 class="ribbon-cont color-black"><div class="ribbon white"></div><b>PLUS SIZE</b></h6>
-                                                        <?php break;
-                                                default:
-                                                        break;
-                                                }
-                                                ?>
-                                                <img src="<?php echo $image['sizes']['product_image_large']; ?>" alt="<?php echo esc_attr($title); ?>"> 
-                                        </div>
-                                        <h5 class="mt-20"><?php echo $title; ?></h5>
-                                        <h4 class="mt-5"><b>$<?php echo $price; ?></b></h4>
-                                        <?php if($link): ?>
-                                                <h6 class="mt-20"><a href="<?php echo esc_url($link); ?>" class="btn-brdr-primary plr-25"><b>Order Now</b></a></h6>
-                                        <?php endif; ?>                                                        
-                                        </div>
-                                </div>
-                        <?php endwhile; ?> 
-                <?php endif; ?>
-                </div>
-            
-                <h6 class="center-text mt-40 mt-sm-20 mb-30">
-                    <a href="#" class="btn-primaryc plr-25"><b>SEE TODAY'S MENU</b></a>
-                </h6>
-            </div>
-        </section>
-    <?php endwhile; ?>
-<?php endif; ?>
-
-
-
-
-
-
-
-
-
-
-<!-- 
-<?php if( have_rows('best_sellers') ): ?>
-    <section class="story-area bg-seller color-white pos-relative">
-        <div class="pos-bottom triangle-up"></div>
-        <div class="pos-top triangle-bottom"></div>
-        <div class="container">
-        <div class="heading">
-                <img class="heading-img" src="<?php bloginfo('template_directory'); ?>/images/heading_logo.png" alt="">
-                <h2>Best Sellers</h2>
-            </div>
-
-            <div class="row">   
-                <?php while( have_rows('best_sellers') ): the_row();
-                    $image = get_sub_field('image');
-                    $title = get_sub_field('title');
-                    $price = get_sub_field('price');
-                    $link = get_sub_field('link');
-                    $offer = get_sub_field('offer');
-                ?>
-                    <div class="col-lg-3 col-md-4 col-sm-6">
-                        <div class="center-text mb-30">
-                            <div class="ïmg-200x mlr-auto pos-relative">
-                                <?php if($offer): ?>                                           
-                                    <h6 class="ribbon-cont"><div class="ribbon primary"></div><b>OFFER</b></h6>
-                                <?php endif; ?>
-                                <img src="<?php echo $image['sizes']['product_image_large']; ?>" alt="<?php echo esc_attr($title); ?>"> 
-                            </div>
-                            <h5 class="mt-20"><?php echo $title; ?></h5>
-                            <h4 class="mt-5"><b>$<?php echo $price; ?></b></h4>
-                            <?php if($link): ?>
-                                <h6 class="mt-20"><a href="<?php echo esc_url($link); ?>" class="btn-brdr-primary plr-25"><b>Order Now</b></a></h6>
-                            <?php endif; ?>                                                        
+                        if ($best_seller_title):
+                    ?>
+                        <div class="heading">
+                            <img class="heading-img" src="<?php echo esc_url($best_seller_icon['url']); ?>" alt="<?php echo esc_attr($best_seller_title); ?>">
+                            <h2><?php echo esc_html($best_seller_title); ?></h2>
                         </div>
-                    </div>
-                <?php endwhile; ?> 
-                
-            </div>
-            <h6 class="center-text mt-40 mt-sm-20 mb-30"><a href="#" class="btn-primaryc plr-25"><b>SEE TODAY'S MENU</b></a></h6>
-        </div>
-    </section>            
-<?php endif; ?>
- -->
+                    <?php endif; ?>
 
+                    <?php
+                    $categories = get_terms('category', array('hide_empty' => false));
 
+                    if ($categories):
+                        ?>
+                        <div class="row">
+                    <?php $categories = get_terms('category', array('hide_empty' => false)); 
+                            $pizza_category = 'pizza';
+                            $category_posts = get_posts(array(
+                                    'post_type' => 'cars',
+                                    'tax_query' => array(
+                                        array(
+                                            'taxonomy' => 'category',
+                                            'field'    => 'slug',
+                                            'terms'    => $pizza_category,
+                                        ),
+                                    ),
+                            ));
 
+                            foreach ($category_posts as $menu_post):
+                            $seller_image = get_field('seller_image', $menu_post->ID);
+                            $title = $menu_post->post_title;
+                            $price = $menu_post->price;
+                            $tags = wp_get_post_tags($menu_post->ID);
+                            $tagname=$tags[0]->name;                        
+                    ?>
+                            <div class="col-lg-3 col-md-4 col-sm-6">
+                                    <div class="center-text mb-30">
+                                    <div class="ïmg-200x mlr-auto pos-relative">
+                                        <?php
 
+                                            switch ($tagname):
+                                            case 'OFFER':
+                                                    ?>
+                                                    <h6 class="ribbon-cont color-white"><div class="ribbon primary"></div><b><?php echo $tagname; ?></b></h6>
+                                                    <?php
+                                                    break;
+                                            case 'SPECIALITY':
+                                                    ?>
+                                                    <h6  class="ribbon-cont color-white"><div class="ribbon secondary"></div><b><?php echo $tagname; ?></b></h6>
+                                                    <?php
+                                                    break;
+                                            case 'PLUS SIZE':
+                                                    ?>
+                                                    <h6 class="ribbon-cont color-black"><div class="ribbon white"></div><b><?php echo $tagname; ?></b></h6>
+                                                    <?php
+                                                    break;
+                                            default:
+                                                    break;
+                                            endswitch;
+                                            ?>
+                                            <img src="<?php echo esc_url($seller_image['url']); ?>" alt="<?php echo esc_attr($title); ?>">
+                                    </div>
+                                    <h5 class="mt-20"><?php echo esc_html($title); ?></h5>
+                                    <h4 class="mt-5"><b>$<?php echo esc_html($price); ?></b></h4>
+                                    </div>
+                            </div>
+                            <?php endforeach; ?>
+                    </div><!-- row-->
+
+                    <?php endif; ?>
+                    <h6 class="center-text mt-40 mt-sm-20 mb-30">
+                        <a href="#" class="btn-primaryc plr-25"><b>SEE TODAY'S MENU</b></a>
+                    </h6>
+                </div>
+            </section>
+        <?php endwhile;
+    endif;
+    ?>
+
+<?php $food_item = get_field('food_menu'); 
+?>
 
 <section>
-        <div class="container">
-                <div class="heading">
-                        <img class="heading-img" src="<?php bloginfo('template_directory');?>/images/heading_logo.png" alt="">
-                        <h2>Our Menu</h2>
-                </div>
-                <?php if(have_rows('our_menu')):?>
-                        <div class="row">
-                                <div class="col-sm-12">
-                                        <ul class="selecton brdr-b-primary mb-70">
-                                                <li><a class="active" href="#" data-select="*"><b>ALL</b></a></li>
-                                                <?php while(have_rows('our_menu')): the_row();
-                                                        $category_name = get_sub_field('category_name');
-                                                        $menu_name = get_sub_field('category');
-                                                ?>
-                                                <li><a href="#" data-select="<?php echo sanitize_title($category_name);?>"><b><?php echo $category_name?></b></a></li>
-                                                <?php if(have_rows('category')):?>
+    <div class="container">
+        <div class="heading">
+            <img class="heading-img" src="<?php echo $food_item['icon']['url']; ?>" alt="icon">
+            <h2><?php echo $food_item['title']; ?></h2>
+        </div>
 
-                                                <?php endif;?>
-                                                <?php endwhile;?>
-                                        </ul>
-                                </div>
+        <div class="row">
+            <div class="col-sm-12">
+                <ul class="selecton brdr-b-primary mb-70">
+                    <li><a class="active" href="#" data-select="*"><b>ALL</b></a></li>
+                    <?php
+                    $categories = get_categories(array(
+                        'taxonomy' => 'category',
+                        'exclude' => get_cat_ID('uncategorized'),
+                    ));
+
+                    foreach ($categories as $category) :
+                    ?>
+                        <li><a href="#" data-select="<?php echo $category->slug; ?>"><b><?php echo $category->name; ?></b></a></li>
+                    <?php endforeach; ?>
+                </ul>
+            </div>
+        </div>
+
+        <div class="row">
+            <?php
+            $menu_posts = get_posts(array('post_type' => 'cars'));
+            $categories = get_terms('category', array('hide_empty' => false));   
+
+            foreach ($categories as $category) :
+            ?>
+                    <?php
+                    $category_posts = get_posts(array(
+                        'post_type' => 'cars',
+                        'tax_query' => array(
+                            array(
+                                'taxonomy' => 'category',
+                                'field'    => 'slug',
+                                'terms'    => $category->slug,
+                            ),
+                        ),
+                    ));
+
+                    foreach ($category_posts as $menu_post) :?>
+                        <div class="col-md-6 food-menu <?php echo $category->slug; ?>">
+
+                       <?php $thumbnail_url = get_the_post_thumbnail_url($menu_post->ID, 'thumbnail');
+                    ?>
+                        <div class="sided-90x mb-30">
+                            <div class="s-left"><img class="br-3" src="<?php echo $thumbnail_url; ?>" alt="Menu Image"></div>
+                            <div class="s-right">
+                                <h5 class="mb-10"><b><?php echo $menu_post->post_title; ?></b><b class="color-primary float-right">$12.00</b></h5>
+                                <p class="pr-70"><?php echo $menu_post->post_content;?></p>
+                            </div>
                         </div>
-                <?php endif;?>
+                </div><!-- food-menu -->
+                <?php endforeach; ?>
 
+            <?php endforeach; ?>
+        </div>
+        <!--row-->
 
-
-
-                <h6 class="center-text mt-40 mt-sm-20 mb-30"><a href="#" class="btn-primaryc plr-25"><b>SEE TODAYS MENU</b></a></h6>
-        </div><!-- container -->
+        <h6 class="center-text mt-40 mt-sm-20 mb-30"><a href="<?php echo $food_item['button_link']; ?>" class="btn-primaryc plr-25"><b><?php echo $food_item['button_text']; ?></b></a></h6>
+    </div><!-- container -->
 </section>
+
 <?php get_footer();?>
--- wp-content/themes/theme/functions.php
+++ wp-content/themes/theme/functions.php
@@ -3,6 +3,7 @@
 
     // Add theme support for featured images
     add_theme_support('post-thumbnails');
+    add_theme_support('custom-logo');
 
 
     function load_stylesheets(){
@@ -51,13 +52,15 @@
     add_image_size('product_image_large', 700, 700, false);
 
 
+
+
+
     add_theme_support('menus');
     register_nav_menus(
         array(
             'top-menu'=>__('Top Menu','theme'),
         ),
     );
-
 
 
 
@@ -93,39 +96,68 @@
 
 
 
-    function my_first_post_type(){
+    //my custome post
+    function my_first_post_type() {
         $args = array(
-            'labels' => array(
-                'name' => 'Cars',
-                'singular_name' => 'Car',
-            ),
-            'hierarchical' =>true,
-            'public'=>true,
+            'hierarchical' => false,
+            'public' => true,
             'has_archive' => true,
-            'supports' =>array('title','editor','thumbnail'),
-
+            'taxonomies' => array('category', 'post_tag'), // Add 'post_tag' for tags
+            'supports' => array('title', 'editor', 'thumbnail', 'tags'), // Separate 'tags' from 'thumbnail'
         );
-
-        register_post_type('cars',$args);
+    
+        register_post_type('cars', $args);
     }
     add_action('init', 'my_first_post_type');
+    
+    
+    
+    
+    
+    
+    
 
-    function my_first_taxonamy(){
-        $args = array(
-            'labels' => array(
-                'name' => 'Brands',
-                'singular_name' => 'Brand',
-            ),
-            'public'=>true,
-            'hierarchical' => true,
-        );
-        register_taxonomy('brands',array('cars'),$args);
-
-    }
-
-  
-
-
-    add_action('init', 'my_first_taxonamy');
+    // function my_first_post_type() {
+    //     $args = array(
+    //         // 'labels' => array(
+    //         //     'name' => 'Products',
+    //         //     'singular_name' => 'Car',
+    //         // ),
+    //         'hierarchical' => false,
+    //         'public' => true,
+    //         'has_archive' => true,
+    //         'taxonomies' => array('category',), 
+    //         'supports' => array('title', 'editor', 'thumbnail', 'tags'), // Add 'tags' to the supports array
+    //     );
+    
+    //     register_post_type('cars', $args);
+    // }
+    // add_action('init', 'my_first_post_type');
+    
+    // function my_first_taxonomy() {
+    //     // Taxonomy for Brands (hierarchical)
+    //     $args_brands = array(
+    //         'labels' => array(
+    //             'name' => 'Category',
+    //             'singular_name' => 'Brand',
+    //         ),
+    //         'public' => true,
+    //         'hierarchical' => true,
+    //     );
+    //     register_taxonomy('brands', array('cars'), $args_brands);
+    
+    //     // Taxonomy for Tags (non-hierarchical, like default post tags)
+    //     $args_tags = array(
+    //         'labels' => array(
+    //             'name' => 'Tags',
+    //             'singular_name' => 'Car Tag',
+    //         ),
+    //         'public' => true,
+    //         'hierarchical' => false, // Set to false for non-hierarchical (like tags)
+    //     );
+    //     register_taxonomy('car_tags', array('cars'), $args_tags);
+    // }
+    // add_action('init', 'my_first_taxonomy');
+    
 
 ?>
