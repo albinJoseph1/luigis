2f8e4da2f72a7937902f2e3156b33d941028a340
Albin Joseph <albin.joseph@yatnam.com>
2024-01-09 16:08:16 +0530

    #3 Header and Footer Completed

--- wp-content/themes/theme/functions.php
+++ wp-content/themes/theme/functions.php
@@ -1,131 +1,58 @@
 <?php
-
-
     // Add theme support for featured images
-    add_theme_support('post-thumbnails');
-
-
-    function load_stylesheets(){
-        wp_register_style("fonts", get_template_directory_uri() ."/fonts/beyond_the_mountains-webfont.css", array(), 1,"all");
-        wp_register_style("bootstrap", get_template_directory_uri() ."/plugin-frameworks/bootstrap.min.css", array(), 1,"all");
-        wp_register_style("swiper", get_template_directory_uri() ."/plugin-frameworks/swiper.css", array(), 1,"all");
-        wp_register_style("ionicons", get_template_directory_uri() ."/fonts/ionicons.css", array(), 1,"all");
-        wp_register_style("styles", get_template_directory_uri() ."/common/styles.css", array(), 1,"all");
-        wp_register_style("custom", get_template_directory_uri() ."/custome.css", array(), 1,"all");
-
-        wp_enqueue_style("fonts");
-        wp_enqueue_style("bootstrap");
-        wp_enqueue_style("swiper");
-        wp_enqueue_style("ionicons");
-        wp_enqueue_style("styles");
-        wp_enqueue_style("custom");
-    }
-
-    add_action('wp_enqueue_scripts','load_stylesheets');
-
-
-
-
-
-
-    function addjs(){
-        wp_deregister_script("jquery");
-        wp_register_script("jquery", get_template_directory_uri() ."/plugin-frameworks/jquery-3.2.1.min.js",array(), 1,1);
-        wp_register_script("bootstrap", get_template_directory_uri() ."/plugin-frameworks/bootstrap.min.js",array(), 1,1);
-        wp_register_script("swiper", get_template_directory_uri() ."/plugin-frameworks/swiper.js",array(), 1,1);
-        wp_register_script("scripts", get_template_directory_uri() ."/common/scripts.js",array(), 1,1);
-        wp_register_script("custom", get_template_directory_uri() ."/custome.js",array(), 1,1);
-
-
-        wp_enqueue_script("jquery");
-        wp_enqueue_script("bootstrap");
-        wp_enqueue_script("swiper");
-        wp_enqueue_script("scripts");
-        wp_enqueue_script("custom");
-    
-    }
-    add_action('wp_enqueue_scripts','addjs');
-
-
+    add_theme_support('custom-logo');
     add_image_size('product_image_small', 100, 100, false);
     add_image_size('product_image_large', 700, 700, false);
-
-
     add_theme_support('menus');
     register_nav_menus(
         array(
             'top-menu'=>__('Top Menu','theme'),
         ),
     );
+    #load CSS Files
+    function load_stylesheets(){
+        wp_register_style("fonts", get_template_directory_uri() ."/fonts/beyond_the_mountains-webfont.css", array(), 1,"all");
+        wp_register_style("bootstrap", get_template_directory_uri() ."/plugin-frameworks/bootstrap.min.css", array(), 1,"all");
+        wp_register_style("swiper", get_template_directory_uri() ."/plugin-frameworks/swiper.css", array(), 1,"all");
+        wp_register_style("ionicons", get_template_directory_uri() ."/fonts/ionicons.css", array(), 1,"all");
+        wp_register_style("styles", get_template_directory_uri() ."/common/styles.css", array(), 1,"all");
 
+        wp_enqueue_style("fonts");
+        wp_enqueue_style("bootstrap");
+        wp_enqueue_style("swiper");
+        wp_enqueue_style("ionicons");
+        wp_enqueue_style("styles");
+    }
+    add_action('wp_enqueue_scripts','load_stylesheets');
+    #load JS Files
+    function addjs(){
+        wp_deregister_script("jquery");
+        wp_register_script("jquery", get_template_directory_uri() ."/plugin-frameworks/jquery-3.2.1.min.js",array(), 1,1);
+        wp_register_script("bootstrap", get_template_directory_uri() ."/plugin-frameworks/bootstrap.min.js",array(), 1,1);
+        wp_register_script("swiper", get_template_directory_uri() ."/plugin-frameworks/swiper.js",array(), 1,1);
+        wp_register_script("progressbar", get_template_directory_uri() ."/plugin-frameworks/progressbar.min.js",array(), 1,1);
+        wp_register_script("waypoints", get_template_directory_uri() ."/plugin-frameworks/jquery.waypoints.min.js",array(), 1,1);
+        wp_register_script("scripts", get_template_directory_uri() ."/common/scripts.js",array(), 1,1);
 
-
-
+        wp_enqueue_script("jquery");
+        wp_enqueue_script("bootstrap");
+        wp_enqueue_script("swiper");
+        wp_enqueue_script("progressbar");
+        wp_enqueue_script("waypoints");
+        wp_enqueue_script("scripts");    
+    }
+    add_action('wp_enqueue_scripts','addjs');
+    #Custom option for header and footer
     if(function_exists('acf_add_options_page'))
     {
         acf_add_options_page(
             array(
-            'page_title' => 'Website Settings',
-            'menu_title' => 'Website Settings',
-            'menu_slug' => 'website-settings',
-            'capability' => 'edit_posts',
-            'icon_url' => 'dashicons-admin-tools'
-            )
-        );
-
-        acf_add_options_sub_page(
-            array(
-            'page_title' => 'Contact Settings',
-            'menu_title' => 'Contact',
-            'parent_slug' => 'website-settings',
-            
-            )
-        );
-        acf_add_options_sub_page(
-            array(
-            'page_title' => 'Design Settings',
-            'menu_title' => 'Design',
-            'parent_slug' => 'website-settings',
-            
+                'page_title' => 'Website Settings',
+                'menu_title' => 'Website Settings',
+                'menu_slug' => 'website-settings',
+                'capability' => 'edit_posts',
+                'icon_url' => 'dashicons-admin-tools'
             )
         );
     }
-
-
-
-    function my_first_post_type(){
-        $args = array(
-            'labels' => array(
-                'name' => 'Cars',
-                'singular_name' => 'Car',
-            ),
-            'hierarchical' =>true,
-            'public'=>true,
-            'has_archive' => true,
-            'supports' =>array('title','editor','thumbnail'),
-
-        );
-
-        register_post_type('cars',$args);
-    }
-    add_action('init', 'my_first_post_type');
-
-    function my_first_taxonamy(){
-        $args = array(
-            'labels' => array(
-                'name' => 'Brands',
-                'singular_name' => 'Brand',
-            ),
-            'public'=>true,
-            'hierarchical' => true,
-        );
-        register_taxonomy('brands',array('cars'),$args);
-
-    }
-
-  
-
-
-    add_action('init', 'my_first_taxonamy');
-
 ?>
--- wp-content/themes/theme/header.php
+++ wp-content/themes/theme/header.php
@@ -1,47 +1,40 @@
 <!DOCTYPE HTML>
 <html lang="en">
-<head>
-        <title>Luigi's</title>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta charset="UTF-8">
-
-        <!-- Font -->
-        <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600" rel="stylesheet">
-
-        
-        <?php wp_head();?>
-
-
-</head>
-<body>
-
-<header>
-        <?php $con_header = get_field('header', 'option'); ?>
-
-        <div class="container">
-                <a class="logo" href="#"><img src="<?php bloginfo('template_directory');?>/images/logo-white.png" alt="Logo"></a>
-
-                <div class="right-area">
-                        <h6><a class="plr-20 color-white btn-fill-primary" href="<?php echo $con_header['button_link']['url']; ?>"><?php echo $con_header['button_text']; ?></a></h6>
-                </div><!-- right-area -->
-
-                <a class="menu-nav-icon" data-menu="#main-menu" href="#"><i class="ion-navicon"></i></a>
-                
-
-
-                <?php 
-                wp_nav_menu(
-                        array(
-                                'theme_loction'=>'top-menu',
-                                'menu_id'=>'main-menu',
-                                'container'=>'ul',
-                                'menu_class'=>'main-menu font-mountainsre',
-                        )
-                );
-                ?>
-               
-
-                <div class="clearfix"></div>
-        </div><!-- container -->
-</header>
+        <head>
+                <title>Luigi's</title>
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+                <meta charset="UTF-8">
+                <!-- Font -->
+                <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600" rel="stylesheet">     
+                <?php wp_head();?>
+        </head>
+        <body>
+                <header>
+                        <?php $con_header = get_field('header', 'option'); ?>
+                        <div class="container">
+                                <?php
+                                $custom_logo_id = get_theme_mod('custom_logo');
+                                if (isset($custom_logo_id) && !empty($custom_logo_id)) {
+                                        $logo_url = wp_get_attachment_image_src($custom_logo_id, 'full')[0];?>
+                                        <a class="logo" href="#"><img src="<?php echo $logo_url; ?>" alt="Logo"></a>
+                                <?php } 
+                                if (isset($con_header['button_link']['url']) && isset($con_header['button_text']) && !empty($con_header['button_text']) && !empty($con_header['button_link']['url'])) { ?>
+                                        <div class="right-area">
+                                                <h6><a class="plr-20 color-white btn-fill-primary" href="<?php echo $con_header['button_link']['url']; ?>"><?php echo $con_header['button_text']; ?></a></h6>
+                                        </div>
+                                <?php } ?>                                                    
+                                <a class="menu-nav-icon" data-menu="#main-menu" href="#"><i class="ion-navicon"></i></a>
+                                <?php 
+                                        wp_nav_menu(
+                                                array(
+                                                        'theme_loction'=>'top-menu',
+                                                        'menu_id'=>'main-menu',
+                                                        'container'=>'ul',
+                                                        'menu_class'=>'main-menu font-mountainsre',
+                                                )
+                                        );
+                                ?>
+                                <div class="clearfix"></div>
+                        </div>
+                </header>
