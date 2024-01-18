a04eef95aa7d12051bf1bcccb6e3110384548333
Albin Joseph <albin.joseph@yatnam.com>
2024-01-18 10:24:31 +0530

    #9 Remove Unwanted variable and correct the indentation

--- wp-content/themes/theme/page-contact.php
+++ wp-content/themes/theme/page-contact.php
@@ -15,14 +15,13 @@ if ($contact_page_top_banner){?>
                         if ($contact_page_top_banner['main_title']){?>
                             <h2 class="mt-30 mb-15"><?php echo $contact_page_top_banner['main_title'];?></h2>
                         <?php } ?>
-                        </div><!-- dplay-tbl-cell -->
+                    </div><!-- dplay-tbl-cell -->
                 </div><!-- dplay-tbl -->
             </div><!-- container -->
         </section>
 <?php }
 
 $contact_section = get_field('contact_section'); 
-$message_section = get_field('message_section'); 
 if ($contact_section['section_title']){?>
     <section class="story-area left-text center-sm-text">
         <div class="container">
@@ -39,7 +38,7 @@ if ($contact_section['section_title']){?>
                     <h5 class="mt-10 mb-30"><?php echo $contact_section['sub_title']; ?></h5>
                 <?php }
                 if ($contact_section['description']) { ?>
-                <p class="mx-w-700x mlr-auto"><?php echo $contact_section['description']; ?></p>
+                    <p class="mx-w-700x mlr-auto"><?php echo $contact_section['description']; ?></p>
                 <?php }?>
             </div>
             <form class="form-style-1 placeholder-1">                        
